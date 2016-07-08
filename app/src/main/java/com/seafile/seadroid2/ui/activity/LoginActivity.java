6cb37ae6c9ace0076db42d3b4b943d09486b6a74
ChangSpring <ChangSpring0728@gmail.com>
2016-07-09 00:17:02 +0800

    写LoginActivity 添加Utils 创建基本的package

---
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.seafile.seadroid2.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.seafile.seadroid2.ui.base.BaseActivity;
+
+/**
+ * Created by Alfred on 2016/7/8.
+ */
+public class LoginActivity extends BaseActivity{
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
---
+++ app/src/main/java/com/seafile/seadroid2/ui/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.seafile.seadroid2.ui.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alfred on 2016/7/8.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
